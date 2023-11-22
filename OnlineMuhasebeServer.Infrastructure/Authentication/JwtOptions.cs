3bd3232600bb2f7579fc635f2a6e145138310f76
Samet Taş <bmsamettas@gmail.com>
2023-11-22 16:58:38 +0300

    Json Web Token

---
+++ OnlineMuhasebeServer.Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineMuhasebeServer.Infrastructure.Authentication
+{
+    public sealed class JwtOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+
+    }
+}
---
+++ OnlineMuhasebeServer.WebApi/Configurations/AuthenticationAndAuthorizationServiceInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using OnlineMuhasebeServer.WebApi.OptionsSetup;
+
+namespace OnlineMuhasebeServer.WebApi.Configurations
+{
+    public class AuthenticationAndAuthorizationServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureOptions<JwtOptionsSetup>();
+
+            services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+        }
+    }
+}
